27f1564234b3053ec199dfcf8622c3cfd9d76ac7
Manish <mmanishrshetty@gmail.com>
2026-08-26 20:30:54 +0530

    feat: add tileset assets, map configuration, and update Vite dev server ignore patterns

---
+++ Map/ts4.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="ts4" tilewidth="32" tileheight="32" tilecount="15606" columns="48">
+ <grid orientation="orthogonal" width="3" height="8"/>
+ <image source="C:/Users/Asus/Downloads/ChatGPT Image Aug 26, 2026, 06_38_34 PM.png" width="1536" height="1024"/>
+</tileset>
---
+++ Map/ts5.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="ChatGPT Image Aug 26, 2026, 06_52_33 PM" tilewidth="16" tileheight="16" tilecount="6144" columns="96">
+ <image source="C:/Users/Asus/Downloads/ChatGPT Image Aug 26, 2026, 06_52_33 PM.png" width="1536" height="1024"/>
+</tileset>
